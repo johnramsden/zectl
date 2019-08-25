373ce15f05d330fd692d2861b0a258bcfb1b8c77
John Ramsden <johnramsden@riseup.net>
2019-08-24 19:06:36 -0700

    Add functions for set & get property parsing

--- lib/libze/libze.c
+++ lib/libze/libze.c
@@ -18,6 +18,158 @@
 
 const char *ZE_PROP_NAMESPACE = "org.zectl";
 
+static libze_error
+parse_property(const char property[static 1], char property_prefix[ZFS_MAXPROPLEN],
+        char property_suffix[ZFS_MAXPROPLEN]) {
+    libze_error ret = LIBZE_ERROR_SUCCESS;
+
+    /* Prefix, just ZE_NAMESPACE if no colon in property
+     * Otherwise ZE_NAMESPACE + part before colon */
+    char prop_prefix[ZFS_MAXPROPLEN];
+
+    // Full ZFS property
+    char temp_prop[ZFS_MAXPROPLEN];
+    if (strlcpy(temp_prop, property, ZFS_MAXPROPLEN) >= ZFS_MAXPROPLEN) {
+        fprintf(stderr, "property '%s' is too long\n", property);
+        return LIBZE_ERROR_MAXPATHLEN;
+    }
+
+    if (strlcpy(property_suffix, property, ZFS_MAXPROPLEN) >= ZFS_MAXPROPLEN) {
+        return LIBZE_ERROR_MAXPATHLEN;
+    }
+
+    char *suffix_value = NULL;
+    if ((suffix_value = strchr(temp_prop, ':')) != NULL) {
+        // Cut at ':'
+        *suffix_value = '\0'; // Prefix in temp_prop
+        suffix_value++;
+        ret = libze_util_concat(ZE_PROP_NAMESPACE, ".", temp_prop, ZFS_MAXPROPLEN, prop_prefix);
+        if (ret != LIBZE_ERROR_SUCCESS) {
+            return ret;
+        }
+
+    } else {
+        if (strlcpy(prop_prefix, ZE_PROP_NAMESPACE, ZFS_MAXPROPLEN) >= ZFS_MAXPROPLEN) {
+            return LIBZE_ERROR_MAXPATHLEN;
+        }
+        suffix_value = temp_prop;
+    }
+
+    if (strlcpy(property_suffix, suffix_value, ZFS_MAXPROPLEN) >= ZFS_MAXPROPLEN) {
+        return LIBZE_ERROR_MAXPATHLEN;
+    }
+    if (strlcpy(property_prefix, prop_prefix, ZFS_MAXPROPLEN) >= ZFS_MAXPROPLEN) {
+        return LIBZE_ERROR_MAXPATHLEN;
+    }
+    return ret;
+}
+
+/**
+ * @brief Given a property with an optional prefix for a bootloader,
+ *        form a ZFS property nvlist
+ * @param[in,out] properties Pre-allocated nvlist to add a property to
+ * @param property Individual property to add to the list
+ * @return @p LIBZE_ERROR_SUCCESS on success,
+ *         @p LIBZE_ERROR_MAXPATHLEN if property is too long,
+ *         @p LIBZE_ERROR_UNKNOWN if no '=' in property
+ * @pre @p properties is allocated and non NULL
+ */
+libze_error
+libze_add_set_property(nvlist_t *properties, const char *property) {
+    libze_error ret = LIBZE_ERROR_SUCCESS;
+
+    // Property value, part after '='
+    char *value = NULL;
+
+    /* Prefix, just ZE_NAMESPACE if no colon in property
+     * Otherwise ZE_NAMESPACE + part before colon */
+    char prop_prefix[ZFS_MAXPROPLEN];
+    // Full resulting ZFS property
+    char prop_full_name[ZFS_MAXPROPLEN];
+    char prop_after_colon[ZFS_MAXPROPLEN];
+
+    if (parse_property(property, prop_prefix, prop_after_colon) != LIBZE_ERROR_SUCCESS) {
+        fprintf(stderr, "property '%s' is too long\n", property);
+        return LIBZE_ERROR_MAXPATHLEN;
+    }
+
+    if ((value = strchr(prop_after_colon, '=')) == NULL) {
+        fprintf(stderr, "missing '=' for property=value argument\n");
+        return LIBZE_ERROR_UNKNOWN;
+    }
+
+    // Cut at '='
+    *value = '\0';
+    value++;
+
+    ret = libze_util_concat(prop_prefix, ":", prop_after_colon, ZFS_MAXPROPLEN, prop_full_name);
+    if (ret != LIBZE_ERROR_SUCCESS) {
+        fprintf(stderr, "property '%s' is too long\n", property);
+        return ret;
+    }
+
+    if (nvlist_exists(properties, prop_full_name)) {
+        fprintf(stderr, "property '%s' specified multiple times\n", property);
+        return LIBZE_ERROR_UNKNOWN;
+    }
+
+    if (nvlist_add_string(properties, prop_full_name, value) != 0) {
+        return LIBZE_ERROR_NOMEM;
+    }
+
+    return LIBZE_ERROR_SUCCESS;
+}
+
+libze_error
+libze_add_get_property(libze_handle *lzeh, nvlist_t **properties, const char *property) {
+    nvpair_t *pair = NULL;
+    libze_error ret = LIBZE_ERROR_SUCCESS;
+
+    /* Prefix, just ZE_NAMESPACE if no colon in property
+     * Otherwise ZE_NAMESPACE + part before colon */
+    char prop_prefix[ZFS_MAXPROPLEN];
+
+    // Full resulting ZFS property
+    char prop_full_name[ZFS_MAXPROPLEN];
+    char prop_after_colon[ZFS_MAXPROPLEN];
+
+    if (parse_property(property, prop_prefix, prop_after_colon) != LIBZE_ERROR_SUCCESS) {
+        return libze_error_set(lzeh, LIBZE_ERROR_MAXPATHLEN,
+                "property '%s' is too long\n", property);
+    }
+
+    ret = libze_util_concat(prop_prefix, ":", prop_after_colon, ZFS_MAXPROPLEN, prop_full_name);
+    if (ret != LIBZE_ERROR_SUCCESS) {
+        return libze_error_set(lzeh, ret, "property '%s' is too long\n", property);
+    }
+
+    for (pair = nvlist_next_nvpair(lzeh->ze_props, NULL); pair != NULL;
+         pair = nvlist_next_nvpair(lzeh->ze_props, pair)) {
+        if (strcmp(nvpair_name(pair), prop_full_name) == 0) {
+            if (nvlist_add_nvpair(*properties, pair) != 0) {
+                return libze_error_set(lzeh, LIBZE_ERROR_UNKNOWN,
+                        "Failed to add property '%s' to list\n", property);
+            }
+        }
+    }
+
+    // Add empty property
+    if (nvlist_empty(*properties)) {
+        nvlist_t *prop_nvl = fnvlist_alloc();
+        if (prop_nvl == NULL) {
+            return  libze_error_set(lzeh, LIBZE_ERROR_NOMEM, "Failed to allocate nvlist\n");
+        }
+        if ((nvlist_add_string(prop_nvl, "value", "-") != 0) ||
+            (nvlist_add_string(prop_nvl, "source", "-") != 0) ||
+            (nvlist_add_nvlist(*properties, prop_full_name, prop_nvl) != 0)) {
+            return libze_error_set(lzeh, LIBZE_ERROR_UNKNOWN,
+                    "Failed to add property '%s' to list\n", property);
+        }
+    }
+
+    return LIBZE_ERROR_SUCCESS;
+}
+
 /**
  * @brief Add a default property
  * @param[out] prop_out Output property list
@@ -1465,6 +1617,27 @@ libze_destroy(libze_handle *lzeh, libze_destroy_options *options) {
     return ret;
 }
 
+/*********************************
+ ************** get **************
+ *********************************/
+
+/**
+ * @brief Get a list of properties off the BE root
+ * @param[in] lzeh Initialized lzeh libze handle
+ * @param[in] properties List of ZFS properties to set
+ * @return @p LIBZE_ERROR_SUCCESS on success,
+ *         @p LIBZE_ERROR_ZFS_OPEN if failure to open @p lzeh->be_root,
+ *         @p LIBZE_ERROR_UNKNOWN if failure to set properties
+ */
+libze_error
+libze_get(libze_handle *lzeh, nvlist_t *properties) {
+    libze_error ret = LIBZE_ERROR_SUCCESS;
+
+//    if (zfs_g)
+
+    return ret;
+}
+
 /**************************************
  ************** activate **************
  **************************************/
@@ -1885,9 +2058,9 @@ err:
     return ret;
 }
 
-/*********************************
- ************** Umount **************
- *********************************/
+/*************************************
+ ************** Unmount **************
+ *************************************/
 
 typedef struct libze_umount_cb_data {
     libze_handle *lzeh;
--- src/zectl_set.c
+++ src/zectl_set.c
@@ -1,86 +1,6 @@
 #include "zectl.h"
 
 /**
- * @brief Given a property with an optional prefix for a bootloader,
- *        form a ZFS property nvlist
- * @param[in,out] properties Pre-allocated nvlist to add a property to
- * @param property Individual property to add to the list
- * @return @p LIBZE_ERROR_SUCCESS on success,
- *         @p LIBZE_ERROR_MAXPATHLEN if property is too long,
- *         @p LIBZE_ERROR_UNKNOWN if no '=' in property
- * @pre @p properties is allocated and non NULL
- */
-static libze_error
-add_property(nvlist_t *properties, const char *property) {
-    libze_error ret = LIBZE_ERROR_SUCCESS;
-
-    // Property value, part after '='
-    char *value = NULL;
-
-    /* Prefix, just ZE_NAMESPACE if no colon in property
-     * Otherwise ZE_NAMESPACE + part before colon */
-    char prop_prefix[ZFS_MAXPROPLEN];
-
-    // Full resulting ZFS property
-    char prop_full_name[ZFS_MAXPROPLEN];
-    char prop_after_colon[ZFS_MAXPROPLEN];
-    char temp_prop[ZFS_MAXPROPLEN];
-
-    if (strlcpy(temp_prop, property, ZFS_MAXPROPLEN) >= ZFS_MAXPROPLEN) {
-        fprintf(stderr, "property '%s' is too long\n", property);
-        return LIBZE_ERROR_MAXPATHLEN;
-    }
-
-    char *suffix_value = NULL;
-    if ((suffix_value = strchr(temp_prop, ':')) != NULL) {
-        // Cut at ':'
-        *suffix_value = '\0'; // Prefix in temp_prop
-        suffix_value++;
-        ret = libze_util_concat(ZE_PROP_NAMESPACE, ".", temp_prop, ZFS_MAXPROPLEN, prop_prefix);
-        if (ret != LIBZE_ERROR_SUCCESS) {
-            fprintf(stderr, "property '%s' is too long\n", property);
-            return ret;
-        }
-    } else {
-        if (strlcpy(prop_prefix, ZE_PROP_NAMESPACE, ZFS_MAXPROPLEN) >= ZFS_MAXPROPLEN) {
-            fprintf(stderr, "property '%s' is too long\n", property);
-            return LIBZE_ERROR_MAXPATHLEN;
-        }
-        suffix_value = temp_prop;
-    }
-
-    if (strlcpy(prop_after_colon, suffix_value, ZFS_MAXPROPLEN) >= ZFS_MAXPROPLEN) {
-        fprintf(stderr, "property '%s' is too long\n", property);
-        return LIBZE_ERROR_MAXPATHLEN;
-    }
-    if ((value = strchr(prop_after_colon, '=')) == NULL) {
-        fprintf(stderr, "missing '=' for property=value argument\n");
-        return LIBZE_ERROR_UNKNOWN;
-    }
-
-    // Cut at '='
-    *value = '\0';
-    value++;
-
-    ret = libze_util_concat(prop_prefix, ":", prop_after_colon, ZFS_MAXPROPLEN, prop_full_name);
-    if (ret != LIBZE_ERROR_SUCCESS) {
-        fprintf(stderr, "property '%s' is too long\n", property);
-        return ret;
-    }
-
-    if (nvlist_exists(properties, prop_full_name)) {
-        fprintf(stderr, "property '%s' specified multiple times\n", property);
-        return LIBZE_ERROR_UNKNOWN;
-    }
-
-    if (nvlist_add_string(properties, prop_full_name, value) != 0) {
-        return LIBZE_ERROR_NOMEM;
-    }
-
-    return LIBZE_ERROR_SUCCESS;
-}
-
-/**
  * set command main function
  * @param lzeh Initialized handle to libze object
  * @param argc As passed to main
@@ -111,7 +31,7 @@ ze_set(libze_handle *lzeh, int argc, char **argv) {
     }
 
     for (int i = 1; i < argc; i++) {
-        if ((ret = add_property(properties, argv[i])) != LIBZE_ERROR_SUCCESS) {
+        if ((ret = libze_add_set_property(properties, argv[i])) != LIBZE_ERROR_SUCCESS) {
             goto err;
         }
     }
