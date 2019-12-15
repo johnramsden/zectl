6c9d1d24aea61a307f052fff25a3226f76e84c36
John Ramsden <johnramsden@riseup.net>
2019-12-15 00:30:47 -0800

    Complete matching boot mount 'What' line

--- lib/libze_plugin/libze_plugin_systemdboot.c
+++ lib/libze_plugin/libze_plugin_systemdboot.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <regex.h>
+#include <stdlib.h>
 
 #include "libze_plugin/libze_plugin_systemdboot.h"
 #include "libze/libze_util.h"
@@ -163,7 +164,155 @@ form_unit_regex(char reg_buf[REGEX_BUFLEN],
     return LIBZE_ERROR_SUCCESS;
 }
 
+/**
+ * @brief Copy binary file into new binary file
+ * @param lzeh
+ * @param file Original file (rb)
+ * @param new_file New file (wb)
+ * @return
+ */
+static libze_error
+do_copy_file(libze_handle *lzeh, FILE *file, FILE *new_file)
+{
+    assert(file != NULL);
+    assert(new_file != NULL);
 
+    while(B_TRUE) {
+        int c = fgetc(file);
+        if(feof(file) != 0) {
+            return LIBZE_ERROR_SUCCESS;
+        }
+        fputc(c, new_file);
+
+        if (ferror (new_file) != 0) {
+            return LIBZE_ERROR_UNKNOWN;
+        }
+
+        fflush(new_file);
+    }
+}
+/**
+ * @brief Copy binary file into new file
+ * @param lzeh
+ * @param file Original filename
+ * @param new_file New filename
+ * @return @p LIBZE_ERROR_SUCCESS on success
+ */
+static libze_error
+copy_file(libze_handle *lzeh, const char *filename, const char *new_filename)
+{
+    FILE *file = NULL;
+    FILE *new_file = NULL;
+
+    libze_error ret = LIBZE_ERROR_SUCCESS;
+
+    file = fopen(filename, "rb");
+    if (file == NULL) {
+        return libze_error_set(lzeh, LIBZE_ERROR_UNKNOWN, "Failed to open %s", filename);
+    }
+
+    new_file = fopen(new_filename, "wb");
+    if (new_file == NULL) {
+        ret = libze_error_set(lzeh, LIBZE_ERROR_UNKNOWN, "Failed to open %s", new_filename);
+        goto err;
+    }
+
+    ret = do_copy_file(lzeh, file, new_file);
+
+err:
+    fclose(file);
+    if (new_file != NULL) {
+        fclose(new_file);
+    }
+
+    return ret;
+}
+
+static libze_error
+form_suffix_string(char suffix[LIBZE_MAX_PATH_LEN],
+                   char efi_mountpoint[LIBZE_MAX_PATH_LEN],
+                   char be[LIBZE_MAX_PATH_LEN]) {
+
+    if ((strlcat(suffix, efi_mountpoint, LIBZE_MAX_PATH_LEN) >= LIBZE_MAX_PATH_LEN) ||
+        (strlcat(suffix, "/env/", LIBZE_MAX_PATH_LEN) >= LIBZE_MAX_PATH_LEN) ||
+        (strlcat(suffix, be, LIBZE_MAX_PATH_LEN) >= LIBZE_MAX_PATH_LEN)) {
+        return LIBZE_ERROR_MAXPATHLEN;
+    }
+
+    return LIBZE_ERROR_SUCCESS;
+}
+
+static libze_error
+file_accessible(libze_handle *lzeh, char unit_buf[LIBZE_MAX_PATH_LEN]) {
+    /* Check if boot.mount is r/w */
+    errno = 0;
+    if (access(unit_buf, R_OK|W_OK) != 0) {
+        switch (errno) {
+            case EACCES:
+                return libze_error_set(lzeh, LIBZE_ERROR_EPERM,
+                        "Boot mountpoint unit %s in not in read/write mode.\n", unit_buf);
+            case ENAMETOOLONG:
+                return libze_error_set(lzeh, LIBZE_ERROR_MAXPATHLEN,
+                        "Boot mountpoint unit path exceeds max path length.\n");
+            default:
+                return libze_error_set(lzeh, LIBZE_ERROR_UNKNOWN,
+                        "Boot mountpoint unit %s could not be accessed.\n", unit_buf);
+        }
+    }
+
+    return LIBZE_ERROR_SUCCESS;
+}
+
+static libze_error
+copy_matched(libze_handle *lzeh,
+            char unit_buf[LIBZE_MAX_PATH_LEN],
+            char replace_line[LIBZE_MAX_PATH_LEN],
+            regex_t *regexp,
+            int tmpfd) {
+
+    int err = 0;
+
+    FILE *tmp = fdopen(tmpfd, "w");
+    if (tmp == NULL) {
+        return libze_error_set(lzeh, LIBZE_ERROR_UNKNOWN,
+                "Failed to open tempfile fd %d.\n", tmpfd);
+    }
+
+    FILE* file = fopen(unit_buf, "r");
+    if (file == NULL) {
+        fclose(tmp);
+        return libze_error_set(lzeh, LIBZE_ERROR_UNKNOWN,
+                "Failed to open unit file fd %s.\n", unit_buf);
+    }
+
+    boolean_t matched = B_FALSE;
+    char line_buf[LIBZE_MAX_PATH_LEN];
+
+    while (fgets(line_buf, LIBZE_MAX_PATH_LEN, file)) {
+        err = regexec(regexp, line_buf, 0, NULL, 0);
+
+        if (err == 0) {
+            matched = B_TRUE;
+            fwrite(replace_line, 1, strlen(replace_line), tmp);
+            fflush(tmp);
+            continue;
+        }
+
+        fwrite(line_buf, 1, strlen(line_buf), tmp);
+        fflush(tmp);
+    }
+
+    fclose(file);
+    fclose(tmp);
+
+    if (!matched) {
+        return libze_error_set(lzeh, LIBZE_ERROR_UNKNOWN,
+                "Failed find regular expression for %s in boot mount unit.\n",
+                replace_line);
+    }
+
+    return LIBZE_ERROR_SUCCESS;
+}
 /**
  * @brief Using boot mountpoint update .mount unit string
  * @param lzeh               libze handle
@@ -184,71 +333,102 @@ update_boot_unit(libze_handle *lzeh, char be_mountpoint[static 2],
      * What=/efi/env/<BE>
      */
     libze_error ret = LIBZE_ERROR_SUCCESS;
+    int interr = 0;
 
+    char active_be[ZFS_MAX_DATASET_NAME_LEN];
+    if (libze_boot_env_name(lzeh->bootfs, ZFS_MAX_DATASET_NAME_LEN, active_be) != 0) {
+        return libze_error_set(lzeh, LIBZE_ERROR_MAXPATHLEN,
+                "Bootfs exceeds max path length.\n");
+    }
+
+    /* Get path to boot.mount */
     char unit_buf[LIBZE_MAX_PATH_LEN];
     ret = form_boot_mountpoint_unit(lzeh, boot_mountpoint, unit_buf);
     if (ret != LIBZE_ERROR_SUCCESS) {
         return ret;
     }
 
-    errno=0;
-    if (access(unit_buf, R_OK|W_OK) != 0) {
-        switch (errno) {
-            case EACCES:
-                return libze_error_set(lzeh, LIBZE_ERROR_EPERM,
-                        "Boot mountpoint unit %s in not in read/write mode.\n", unit_buf);
-            case ENAMETOOLONG:
-                return libze_error_set(lzeh, LIBZE_ERROR_MAXPATHLEN,
-                        "Boot mountpoint unit path exceeds max path length.\n", unit_buf);
-            default:
-                return libze_error_set(lzeh, LIBZE_ERROR_UNKNOWN,
-                        "Boot mountpoint unit %s could not be accessed.\n", unit_buf);
-        }
+    if ((ret = file_accessible(lzeh, unit_buf)) != LIBZE_ERROR_SUCCESS) {
+        return ret;
     }
 
     /* Setup regular expression */
     char reg_buf[REGEX_BUFLEN];
     char suffix[LIBZE_MAX_PATH_LEN];
+    char replace_suffix[LIBZE_MAX_PATH_LEN];
 
-    /* Read in boot unit */
-    // TODO: Check 'Where'
-
-    // TODO Replace default
-    if ((strlcat(suffix, efi_mountpoint, LIBZE_MAX_PATH_LEN) >= LIBZE_MAX_PATH_LEN) ||
-        (strlcat(suffix, "/env/", LIBZE_MAX_PATH_LEN) >= LIBZE_MAX_PATH_LEN) ||
-        (strlcat(suffix, "default", LIBZE_MAX_PATH_LEN) >= LIBZE_MAX_PATH_LEN)) {
+    /* Create 'What=' suffix and replacement suffix */
+    ret = form_suffix_string(suffix, efi_mountpoint, active_be);
+    ret = (ret != 0) ? ret : form_suffix_string(replace_suffix, efi_mountpoint, be_name);
+    if (ret != 0) {
         ret = libze_error_set(lzeh, LIBZE_ERROR_MAXPATHLEN,
-                "EFI mountpoint and be name path exceeds max path length.\n");
+                "Unit EFI path exceeds max path length.\n");
         goto err;
     }
 
+    /* Create a regex to find 'What' in boot.mount */
     ret = form_unit_regex(reg_buf, "What", suffix);
     if (ret != 0) {
         ret = libze_error_set(lzeh, LIBZE_ERROR_MAXPATHLEN,
                 "Regex exceeds max path length.\n");
         goto err;
     }
-
     regex_t regexp;
-    int re_ret = regcomp(&regexp, reg_buf, 0);
-    if (re_ret != 0) {
+    interr = regcomp(&regexp, reg_buf, 0);
+    if (interr != 0) {
         char buf[LIBZE_MAX_ERROR_LEN];
-        (void) regerror(re_ret, &regexp, buf, LIBZE_MAX_ERROR_LEN);
+        (void)regerror(interr, &regexp, buf, LIBZE_MAX_ERROR_LEN);
         ret = libze_error_set(lzeh, LIBZE_ERROR_UNKNOWN,
                 "Regex %s failed to compile:\n%s\n", reg_buf, buf);
         goto err;
     }
 
-    char line_buf[LIBZE_MAX_PATH_LEN];
-    FILE* file = fopen(unit_buf, "r");
-    while (fgets(line_buf, LIBZE_MAX_PATH_LEN, file)) {
-        re_ret = regexec(&regexp, line_buf, 0, NULL, 0);
-        if (re_ret == 0) {
-            // TODO: MATCH
-            printf("%s", line_buf);
-        }
+    /* Create a tempfile to manipulate before replacing original */
+    char tmpfile[LIBZE_MAX_PATH_LEN];
+    interr = libze_util_concat(SYSTEMD_SYSTEM_UNIT_PATH, "/",
+            ".zectl-sdboot.XXXXXX", LIBZE_MAX_PATH_LEN, tmpfile);
+    if (interr != 0) {
+        ret = libze_error_set(lzeh, LIBZE_ERROR_MAXPATHLEN,
+                "Temporary boot mount unit exceeds max path length.\n");
+        goto err;
     }
-    fclose(file);
+
+    /* Create the replacement line */
+    char replace_line[LIBZE_MAX_PATH_LEN];
+    interr = libze_util_concat("What=", replace_suffix,
+            "\n", LIBZE_MAX_PATH_LEN, replace_line);
+    if (interr != 0) {
+        ret = libze_error_set(lzeh, LIBZE_ERROR_MAXPATHLEN,
+                "Unit EFI path exceeds max path length.\n");
+        goto err;
+    }
+
+    int fd = mkstemp(tmpfile);
+    if (fd == -1) {
+        ret = libze_error_set(lzeh, LIBZE_ERROR_UNKNOWN,
+                "Failed to create temporary file\n");
+        goto err;
+    }
+
+    ret = copy_matched(lzeh, unit_buf, replace_line, &regexp, fd);
+    if (ret != LIBZE_ERROR_SUCCESS) {
+        ret = libze_error_set(lzeh, LIBZE_ERROR_UNKNOWN,
+                "Failed find regular expression for %s in boot mount unit.\n",
+                replace_line);
+        goto err;
+    }
+
+    errno = 0;
+    /* Use rename for atomicity */
+    interr = rename(tmpfile, unit_buf);
+    if (interr != 0) {
+        // TODO: Better error message from errno
+        remove(tmpfile);
+        ret = libze_error_set(lzeh, LIBZE_ERROR_UNKNOWN,
+                "Failed to replace boot mount unit %s.\n",
+                unit_buf);
+        goto err;
+    }
 
 err:
     regfree(&regexp);
@@ -259,6 +439,7 @@ err:
  * @brief Run mid-activate hook
  * @param lzeh Initialized libze handle
  * @param be_mountpoint
+ * @param be_name New be
  * @return Non-zero on failure
  */
 libze_error
