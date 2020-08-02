469a5e63d886a83b7fde2274617bef8fa958c7d3
John Ramsden <johnramsden@riseup.net>
2020-08-01 23:54:40 -0700

    Bump version to 0.1.2
    Fixes #27

--- src/zectl.c
+++ src/zectl.c
@@ -15,7 +15,7 @@
 #include <string.h>
 
 char const *const ZE_PROGRAM = "zectl";
-char const *const ZECTL_VERSION = "0.1.1";
+char const *const ZECTL_VERSION = "0.1.2";
 
 /* Function pointer to command */
 typedef libze_error (*command_func)(libze_handle *lzeh, int argc, char **argv);
